891f4225722f0278610ee51a0ac409516110d867
gabis <gabrielvenanc@yahoo.com>
2021-06-11 19:51:33 -0300

    versão final do codigo

--- classifiers.py
+++ classifiers.py
@@ -4,48 +4,72 @@ import re
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import sklearn
 import tensorflow as tf
+import random
 from unidecode import unidecode
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Sequential
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn import metrics
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, precision_recall_curve, \
-    average_precision_score, roc_curve, plot_roc_curve
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, plot_roc_curve
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import KFold
 from sklearn.metrics import auc
+import codecs
 
+
+path = 'C:/Users/gabis/Documents/tcc/Fake_News_Ckassifiers/input/validation.csv'
+folder = 'C:/Users/gabis/Documents/tcc/Fake_News_Ckassifiers/input/dataToProcess'
+GLOVE_DIR = 'C:/Users/gabis/PycharmProjects/Glove/input/glove_s50.txt'
 
 def preProcess():
+    #metodo utilizado apenas uma vez para pre processamento da base
     stop_words = nltk.corpus.stopwords.words("portuguese")
-    path = "C:/Fake.br-Corpus/full_texts/filtradas/teste/"
-    for file in os.listdir(path):
-        word_list = open(path+file, encoding="utf-8")
+    folder
+    for file in os.listdir(folder):
+        word_list = open(folder + file,encoding='utf-8')
         line = word_list.read()
         words = line.split()
         for r in words:
-
             if not r in stop_words:
                 r = re.sub(r'[^\w\s]', ' ', r)
                 r = re.sub("\d+", ' ', r)
-                appendFile = open('processadas.csv', 'a', encoding="utf-8")
+                appendFile = open('validation.csv', 'a', encoding="utf-8")
                 appendFile.write(" " + unidecode(r.lower()))
                 appendFile.close()
-        appendFile = open('processadas.csv', 'a', encoding="utf-8")
-        appendFile.write(";true\n")
-        appendFile.close()
+        appendFile = open('validation.csv', 'a',encoding='utf-8')
+        appendFile.write(";REAL\n")
+
+def preProcess2():
+    #metodo utilizado apenas uma vez para pre processamento da base
+
+    file1 = codecs.open(path, 'r', 'utf-8')
+    i = 0
+    for line in file1.readlines():
+       appendFile = open( 'path' + i.__str__() +'.csv', 'a', encoding='utf-8')
+       appendFile.write(line)
+       appendFile.close()
+       i= i+1
+
+
+def ramdomizeBase():
+    # metodo utilizado apenas uma vez para embaralhar a base
+    destin_folder = 'C:/Users/gabis/Documents/tcc/Fake_News_Ckassifiers/input/validation2.csv'
+    with open(path, 'r', encoding='utf-8') as r, open(destin_folder, 'w', encoding='utf-8') as w:
+        data = r.readlines()
+        header, rows = data[0], data[1:]
+        random.shuffle(rows)
+        rows = '\n'.join([row.strip() for row in rows])
+        w.write(header + rows)
 
 
 def get_base(algorithm):
-    df = pd.read_csv('C:/Users/gabis/PycharmProjects/Glove/input/processadas.csv')
-
+    #metodo utilizado dentro dos algoritimos para utilizar a base de noticias
+    df = pd.read_csv(path)
     if algorithm == "Glove":
         df.head()
         df.info()
@@ -67,106 +91,22 @@ def get_base(algorithm):
 
         no_of_fakes = df.loc[df['label'] == 'FAKE'].count()[0]
         no_of_trues = df.loc[df['label'] == 'REAL'].count()[0]
-        
+
         print(df['label'].unique())
         print(no_of_fakes)
         print(no_of_trues)
-        
+
         y = df['label'].values
         vectorizer = CountVectorizer()
         x = vectorizer.fit_transform(df['text'].values)
+        print(x)
         x = x.toarray()
         return x, y
 
 
 
-def glove():
-    x, y = get_base("Glove")
-    MAX_NB_WORDS = 100000  # maximo de palavras tokenrizadas
-    MAX_SEQUENCE_LENGTH = 1000  # max length of each sentences, including padding
-    VALIDATION_SPLIT = 0.2  # 20% of data for validation (not used in training)
-    EMBEDDING_DIM = 50  # embedding dimensions for word vectors
-    GLOVE_DIR = "C:/Users/gabis/PycharmProjects/Glove/input/glove_s50.txt"
-
-    tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
-    tokenizer.fit_on_texts(x)
-    sequences = tokenizer.texts_to_sequences(x)
-    word_index = tokenizer.word_index
-    print('Vocabulary size:', len(word_index))
-
-    data = pad_sequences(sequences, padding='post', maxlen=MAX_SEQUENCE_LENGTH)
-    print('Shape of data tensor:', data.shape)
-    print('Shape of label tensor:', y.shape)
-
-    indices = np.arange(data.shape[0])
-    np.random.shuffle(indices)
-    data = data[indices]
-    labels = y[indices]
-
-    num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
-    x_train = data[: -num_validation_samples]
-    y_train = labels[: -num_validation_samples]
-    x_val = data[-num_validation_samples:]
-    y_val = labels[-num_validation_samples:]
-    print('Number of entries in each category:')
-    print('training: ', y_train.sum(axis=0))
-    print('validation: ', y_val.sum(axis=0))
-
-    print('Tokenized sentences: \n', data[10])
-    print('One hot label: \n', labels[10])
-
-    embeddings_index = {}
-    f = open(GLOVE_DIR, encoding='utf8')
-    print('Loading Glove from:', GLOVE_DIR, '…', end='')
-    for line in f:
-        try:
-            values = line.split()
-            word = values[0]
-            embeddings_index[word] = np.asarray(values[1:], dtype='float32')
-        except:
-            print()
-    f.close()
-    print("Done.\n Proceeding with Embedding Matrix…", end="")
-    embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
-    for word, i in word_index.items():
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
-            embedding_matrix[i] = embedding_vector
-    print("Completed!")
-
-    model = Sequential()
-    model.add(Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32'))
-    model.add(Embedding(len(word_index) + 1,
-                        EMBEDDING_DIM,
-                        weights=[embedding_matrix],
-                        input_length=MAX_SEQUENCE_LENGTH,
-                        trainable=False,
-                        name='embeddings'))
-    model.add(LSTM(60, return_sequences=True, name='lstm_layer'))
-    model.add(GlobalMaxPool1D())
-    model.add(Dropout(0.1))
-    model.add(Dense(50, activation="relu"))
-    model.add(Dropout(0.1))
-    model.add(Dense(2, activation="sigmoid"))
-
-    model.compile(optimizer='adam', loss='binary_crossentropy',
-                  metrics=['accuracy',tf.keras.metrics.AUC(from_logits=True)])
-
-    history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_data=(x_val, y_val))
-
-    random_num = np.random.randint(0, 100)
-    test_data = x[random_num]
-    test_label = y[random_num]
-    clean_test_data = test_data
-    test_tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
-    test_tokenizer.fit_on_texts(clean_test_data)
-    test_sequences = tokenizer.texts_to_sequences(clean_test_data)
-    word_index = test_tokenizer.word_index
-    test_data_padded = pad_sequences(test_sequences, padding='post', maxlen=MAX_SEQUENCE_LENGTH)
-
-    print('Sample data:', x[0], y[0])
-
 def naive_bayes():
+    #metodo simples de implementação de naive bayes
     x, y = get_base("Naive")
     x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, test_size=0.20, random_state=11)
 
@@ -182,14 +122,13 @@ def naive_bayes():
     print(classification_report(y_test, svc_pred))
 
 def svm():
-
+    #metodo simples de implementação de svm
     x, y = get_base("SVM")
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=1)
-    model = LinearSVC()
+    model = MultinomialNB()
 
-    model_svc = model.fit(x_train, y_train)
+    model_svc = model.fit(y_test, y_train)
     svc_pred = model_svc.predict(x_test)
-
     print("Accuracy do SVM Classifier: {}%".format(round(accuracy_score(y_test, svc_pred) * 100, 2)))
     print("\nMatrix de confusão SVM:\n")
     print(confusion_matrix(y_test, svc_pred))
@@ -197,15 +136,18 @@ def svm():
     print(classification_report(y_test, svc_pred))
 
 def naive_bayesCross():
+    # metodo de implementação de naive bayes em K-fold
     model = MultinomialNB()
     k_fold_cross("Naive", model)
 
 def svmCross():
+    # metodo de implementação de svm em K-fold
     model = LinearSVC()
     k_fold_cross("SVM", model)
 
 def k_fold_cross(algorithm,model):
-    aucs_score =[]
+    #metodo do kFold
+
     acc_score = []
     X, y = get_base(algorithm)
     k = 10
@@ -227,23 +169,22 @@ def k_fold_cross(algorithm,model):
 
 
 
-def draw_cv_roc_curveGlove():
+def glove():
+    #metodo para implementação do glove
     x, y = get_base("Glove")
-    MAX_NB_WORDS = 100000  # maximo de palavras tokenrizadas
-    MAX_SEQUENCE_LENGTH = 1000  # max length of each sentences, including padding
-    VALIDATION_SPLIT = 0.2  # 20% of data for validation (not used in training)
-    EMBEDDING_DIM = 50  # embedding dimensions for word vectors
-    GLOVE_DIR = "C:/Users/gabis/PycharmProjects/Glove/input/glove_s50.txt"
+    MAX_NB_WORDS = 100000
+    MAX_SEQUENCE_LENGTH = 1000
+    VALIDATION_SPLIT = 0.2
+    EMBEDDING_DIM = 50
 
     tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
     tokenizer.fit_on_texts(x)
     sequences = tokenizer.texts_to_sequences(x)
     word_index = tokenizer.word_index
-    print('Vocabulary size:', len(word_index))
+    print('Tamanho do vocabulario:', len(word_index))
 
     data = pad_sequences(sequences, padding='post', maxlen=MAX_SEQUENCE_LENGTH)
-    print('Shape of data tensor:', data.shape)
-    print('Shape of label tensor:', y.shape)
+
 
     indices = np.arange(data.shape[0])
     np.random.shuffle(indices)
@@ -255,16 +196,12 @@ def draw_cv_roc_curveGlove():
     y_train = labels[: -num_validation_samples]
     x_val = data[-num_validation_samples:]
     y_val = labels[-num_validation_samples:]
-    print('Number of entries in each category:')
-    print('training: ', y_train.sum(axis=0))
-    print('validation: ', y_val.sum(axis=0))
-
-    print('Tokenized sentences: \n', data[10])
-    print('One hot label: \n', labels[10])
+    print('Numeros em categorias:')
+    print('treino: ', y_train.sum(axis=0))
+    print('validacao: ', y_val.sum(axis=0))
 
     embeddings_index = {}
     f = open(GLOVE_DIR, encoding='utf8')
-    print('Loading Glove from:', GLOVE_DIR, '…', end='')
     for line in f:
         try:
             values = line.split()
@@ -273,13 +210,13 @@ def draw_cv_roc_curveGlove():
         except:
             print()
     f.close()
-    print("Done.\n Proceeding with Embedding Matrix…", end="")
+    print("Processando embending matrix", end="")
     embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
     for word, i in word_index.items():
         embedding_vector = embeddings_index.get(word)
         if embedding_vector is not None:
             embedding_matrix[i] = embedding_vector
-    print("Completed!")
+    print("Finalizado!")
 
     model = Sequential()
     model.add(Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32'))
@@ -302,63 +239,52 @@ def draw_cv_roc_curveGlove():
                               tf.keras.metrics.TruePositives(name='tp'),
                               tf.keras.metrics.FalsePositives(name='fp'),
                               tf.keras.metrics.TrueNegatives(name='tn'),
-                              tf.keras.metrics.FalseNegatives(name='fn')])
-    print(tf.math.confusion_matrix(y_val, model.predict(x_val)))
+                              tf.keras.metrics.FalseNegatives(name='fn'),
+                              tf.keras.metrics.Recall()])
 
-    history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_data=(x_val, y_val))
+
+    history = model.fit(x_train, y_train, epochs=10, batch_size=128,  validation_data=(x_val, y_val))
     result = model.evaluate(x_val, y_val)
-    accuracy = result[1]
-
-    print(f"[+] Accuracy: {accuracy * 100:.2f}%")
     print(pd.DataFrame(history.history))
-    #print(tf.math.confusion_matrix(y_val, result))
-    train_predictions_baseline = model.predict(x_train, batch_size=128)
-    test_predictions_baseline = model.predict(x_val, batch_size=128)
-    fp, tp, _ = sklearn.metrics.roc_curve(labels, result)
+    accuracy = result[1]
+    print(pd.DataFrame(result))
+    f1_score = 2 * (accuracy * result[7]) / (accuracy + result[7])
+    print("f1 score:" + f1_score.__str__())
+    print(f"[+] Accuracy: {accuracy * 100:.2f}%")
 
 
-
-def plot_cm(labels, predictions, p=0.5):
-  cm = confusion_matrix(labels, predictions > p)
-  plt.figure(figsize=(5,5))
-  sns.heatmap(cm, annot=True, fmt="d")
-  plt.title('Confusion matrix @{:.2f}'.format(p))
-  plt.ylabel('Actual label')
-  plt.xlabel('Predicted label')
-
-  print('Legitimate Transactions Detected (True Negatives): ', cm[0][0])
-  print('Legitimate Transactions Incorrectly Detected (False Positives): ', cm[0][1])
-  print('Fraudulent Transactions Missed (False Negatives): ', cm[1][0])
-  print('Fraudulent Transactions Detected (True Positives): ', cm[1][1])
-  print('Total Fraudulent Transactions: ', np.sum(cm[1]))
 
 def naive_bayes_all_validations():
+    #metodo com k-fold e plot de curva roc do algoritimo naive bayes
     draw_cv_roc_curve(MultinomialNB())
 
 def svm_all_validations():
+    #metodo com k-fold e plot de curva roc do algoritimo svm
     draw_cv_roc_curve(LinearSVC())
 
 def draw_cv_roc_curve(model):
 
+    #metodo com k-fold e plot de curva roc
+
     X, y = get_base("")
     classifier = model
 
-    # #############################################################################
-    # Classification and ROC analysis
-    # Run classifier with cross-validation and plot ROC curves
+
     k = 10
     cv = StratifiedKFold(n_splits=k)
     acc_score = []
     tprs = []
     aucs = []
     mean_fpr = np.linspace(0, 1, 100)
-
     fig, ax = plt.subplots()
     for i, (train, test) in enumerate(cv.split(X, y)):
         classifier.fit(X[train], y[train])
         viz = plot_roc_curve(classifier, X[test], y[test],
                              name='ROC fold {}'.format(i),
                              alpha=0.3, lw=1, ax=ax)
+        print(mean_fpr)
+        print(viz.fpr)
+        print(viz.tpr)
         interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
         interp_tpr[0] = 0.0
         tprs.append(interp_tpr)
@@ -372,9 +298,10 @@ def draw_cv_roc_curve(model):
         print("Report")
         print(classification_report(y[test], pred_values))
 
+
+
     ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
             label='Chance', alpha=.8)
-
     mean_tpr = np.mean(tprs, axis=0)
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
---
+++ main.py
@@ -0,0 +1,11 @@
+import classifiers
+#metodos svm
+classifiers.svm()
+classifiers.svmCross()
+classifiers.svm_all_validations()
+#metodos naive bayes
+classifiers.naive_bayes()
+classifiers.naive_bayesCross()
+classifiers.naive_bayes_all_validations()
+#metodo glove
+classifiers.glove()
